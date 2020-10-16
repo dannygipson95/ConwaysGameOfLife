6e04714733c30b571260de15b930d21d881d5273
Daniel Gipson <dannygipson95@gmail.com>
2020-10-15 21:35:01 -0400

    created grid and worked on styling

--- conways-game-of-life/src/App.js
+++ conways-game-of-life/src/App.js
@@ -1,32 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
-import {SideBySide, GameBox} from './styles.js';
+import {SideBySide, GameBox, CellBox, Title} from './styles.js';
 import './App.css';
+
+class Cell{
+  constructor(row, column){
+  this.row = row;
+  this.column = column;
+  this.id = `c${this.row}:${this.column}`
+  this.alive = false;
+  }
+}
+
+const rows = [];
+for (let i = 0; i < 30; i++){
+  const newRow = [];
+  for (let j = 0; j < 30; j++){
+    newRow.push(new Cell(i, j))
+  }
+  rows.push(newRow)  
+}
+console.log(rows);
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <Title>
         <h1>Conway's Game of Life</h1>
-      </header>
+      </Title>
       <SideBySide>
-        <GameBox>
-        Grid
-        </GameBox>
-        <ul>
-          <h1>Presets</h1>
-          <li>Preset 1</li>
-          <li>Preset 2</li>
-          <li>Preset 3</li>
-          <li>Preset 4</li>
-        </ul>
         <div>
-          <h1>Rules</h1>
+          <button>Start</button>
+          <button>Stop</button>
+          <GameBox>
+          {rows.map(row=>{
+            return(row.map(cell=>{
+              return(
+                <CellBox id={cell.id}></CellBox>
+              )
+            }))
+          })}
+          </GameBox>
+        </div>
+        <div>
           <ul>
-            <li>Rule 1</li>
-            <li>Rule 2</li>
-            <li>Rule 3</li>
+            <h1>Presets</h1>
+            <li>Preset 1</li>
+            <li>Preset 2</li>
+            <li>Preset 3</li>
+            <li>Preset 4</li>
           </ul>
+          <div>
+            <h1>Rules</h1>
+            <ul>
+              <li>Rule 1</li>
+              <li>Rule 2</li>
+              <li>Rule 3</li>
+            </ul>
+          </div>
         </div>
       </SideBySide>
       <h2>About the Game</h2>
--- conways-game-of-life/src/styles.js
+++ conways-game-of-life/src/styles.js
@@ -4,7 +4,28 @@ export const SideBySide = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-around;
+    width: 90%;
+    margin: 0 5%;
 `
 export const GameBox = styled.div`
-    border: 2px solid black;
+    border: 1px solid black;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0;
+    height: 300px;
+    width: 300px;
+`
+
+export const CellBox = styled.div`
+    background: gainsboro;
+    height: 0;
+    padding: 4px;
+    border: 1px solid black;
+    margin: 0;
+`
+
+export const Title = styled.header`
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    width: 90%;
+    margin: 0 5%;
 `
